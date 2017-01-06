8e24f993239627c1bdbfc1ff48f817b741d1b69d
HenryLee <lee0906@hotmail.com>
2017-01-06 21:36:57 +1100

    Add the basic insert operation

--- rb_tree.h
+++ rb_tree.h
@@ -19,8 +19,9 @@ class rb_tree {
   typedef Alloc allocator_type;
   typedef value_type& reference;
   typedef const value_type& const_reference;
-  typedef std::allocator_traits<allocator_type>::pointer pointer;
-  typedef std::allocator_traits<allocator_type>::const_pointer const_pointer;
+  typedef typename std::allocator_traits<allocator_type>::pointer pointer;
+  typedef typename std::allocator_traits<allocator_type>::const_pointer
+    const_pointer;
   class iterator;
   typedef iterator const_iterator;
   class reverse_iterator;
@@ -32,10 +33,15 @@ class rb_tree {
   struct rb_tree_node;
   typedef rb_tree_node* node_ptr;
 
-  typedef bool rb_tree_color;
+  enum rb_tree_color { red_, black_ };
+  static constexpr node_ptr nil = 0;
 
   typedef std::allocator_traits<allocator_type> alloc_traits;
-  typedef typename alloc_traits::template rebind_traits<struct rb_tree_node> node_alloc_traits;
+  typedef typename alloc_traits::template rebind_traits<struct rb_tree_node>
+    node_alloc_traits;
+  typename node_alloc_traits::allocator_type alloc_;
+
+  key_compare comp_;
 
  public:
   std::pair <iterator, bool> insert(const value_type& val);
@@ -47,10 +53,10 @@ class rb_tree {
 
   iterator find (const key_type& val);
 
-  iterator begin();
-  const_iterator begin() const;
-  iterator end();
-  const_iterator end() const;
+  iterator begin() { return iterator(begin_); }
+  const_iterator begin() const { return iterator(begin_); }
+  iterator end() { return iterator(end_); }
+  const_iterator end() const { return iterator(end_); }
   const_iterator cbegin() const;
   const_iterator cend() const;
 
@@ -64,15 +70,41 @@ class rb_tree {
   size_type size() const { return size_; }
   bool empty() const { return size_ == 0; }
 
-  rb_tree() : root_(nil), size_(0) { }
+  rb_tree() : root_(nil), begin_(nil), size_(0) {
+    end_ = node_alloc_traits::allocate(alloc_, 1);
+    end_->parent = end_;
+    end_->left = root_;
+    end_->right = root_;
+  }
 
   class iterator {
    public:
 	const T& operator*() const { return ptr_->key; }
 	const T* operator->() const { return &(operator*()); }
 
-	iterator& operator++();
-	iterator operator++(int);
+	iterator& operator++() {
+      if (ptr_->right != nil) {
+        ptr_ = ptr_->right;
+        while (ptr_->left != nil) {
+          ptr_ = ptr_->left;
+        }
+      } else {
+        node_ptr tmp = ptr_->parent;
+        while (tmp->left != ptr_) {
+          ptr_ = tmp;
+          tmp = ptr_->parent;
+        }
+        ptr_ = tmp;
+      }
+      return *this;
+    }
+
+	iterator operator++(int) {
+      iterator tmp(ptr_);
+      ++*this;
+      return tmp;
+    }
+
 	iterator& operator--();
 	iterator operator--(int);
 
@@ -83,6 +115,9 @@ class rb_tree {
 	friend inline bool operator!=(const iterator& x, const iterator& y) {
 	  return !(x.ptr_ == y.ptr_);
 	}
+
+	iterator() { }
+	iterator(node_ptr x) { ptr_ = x; }
 
    private:
 	node_ptr ptr_;
@@ -98,19 +133,18 @@ class rb_tree {
 
  protected:
   struct rb_tree_node {
-	T key;
+	value_type key;
     rb_tree_color color;
     node_ptr parent;
     node_ptr left;
     node_ptr right;
 
-	rb_tree_node(const T& val) : key(val) { }
+	rb_tree_node(const value_type& val) : key(val) { }
   };
 
-  static const node_ptr nil = 0;
-  static const rb_tree_color color_red = false;
-  static const rb_tree_color color_black = true;
   node_ptr root_;
+  node_ptr begin_;
+  node_ptr end_;
   size_type size_;
 
   static node_ptr MinimumNode(node_ptr x);
@@ -119,20 +153,83 @@ class rb_tree {
   static node_ptr SuccessorNode(node_ptr x);
   static node_ptr PredecessorNode(node_ptr x);
 
-  std::pair <node_ptr, bool> InsertValue(node_ptr y, node_ptr z);
-  node_ptr DeleteValue(node_ptr x);
-
  private:
-  std::pair <node_ptr, bool> InsertNode(node_ptr y, node_ptr z);
-  node_ptr DeleteNode(node_ptr x);
+  void InsertFixup(node_ptr y, node_ptr z);
+  void DeleteNode(node_ptr x);
 
   void Transplant(node_ptr u, node_ptr v);
   void LeftRotate(node_ptr x);
   void RightRotate(node_ptr x);
-  void InsertFixup(node_ptr x);
   void DeleteFixup(node_ptr x);
 
 };
+
+template <class T, class C, class A>
+std::pair <typename rb_tree<T, C, A>::iterator, bool>
+rb_tree<T, C, A>::insert(const value_type& val) {
+  node_ptr z = node_alloc_traits::allocate(alloc_, 1);
+  node_ptr x = root_;
+  node_ptr y = end_;
+
+  z->key = val;
+  z->left = nil;
+  z->right = nil;
+
+  ++size_;
+
+  bool comp = true;
+
+  while (x != nil) {
+    y = x;
+    comp = comp_(val, x->key);
+    x = comp ? x->left : x->right;
+  }
+
+  node_ptr j = y;
+  if (comp) {
+    y->left = z;
+    z->parent = y;
+
+    if (y == begin_) {
+      /* begin */
+      begin_ = z;
+
+      return std::pair<iterator, bool>(iterator(z), true);
+    } else if (y == end_) {
+      /* root */
+      root_ = z;
+      y->right = z;
+      begin_ = z;
+
+      return std::pair<iterator, bool>(iterator(z), true);
+    } else {
+      /* decrement j */
+      node_ptr tmp = j->parent;
+      while (j == tmp->left) {
+        j = tmp;
+        tmp = j->parent;
+      }
+    }
+  } else {
+    y->right = z;
+    z->parent = y;
+  }
+
+  if (comp_(j->key, val))
+    return std::pair<iterator, bool>(iterator(z), true);
+  else
+    return std::pair<iterator, bool>(iterator(j), false);
+
+}
+
+template <class T, class C, class A>
+void rb_tree<T, C, A>::InsertFixup(typename rb_tree<T, C, A>::node_ptr y,
+                                   typename rb_tree<T, C, A>::node_ptr z) {
+  z->color = red_;
+
+  return z;
+}
+
 
 } // namespace rbtree
 
